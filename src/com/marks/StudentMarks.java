32fbcac137e444e209dc0c3cd5e3eeeaa9c9f528
Neeraj7979 <Imnsinha77@outlook.com>
2022-08-21 20:44:29 +0530

    Declared attribute and getter and setter method for StudentMarks class

---
+++ src/com/marks/StudentMarks.java
@@ -0,0 +1,76 @@
+package com.marks;
+
+public class StudentMarks {
+    private int math;
+    private double biology;
+    private int physics;
+    private double chemistry;
+    private int businessStudies;
+    private int finance;
+    private int accountancy;
+    private int totalMarks;
+
+    public int getMath() {
+        return math;
+    }
+
+    public void setMath(int math) {
+        this.math = math;
+    }
+
+    public double getBiology() {
+        return biology;
+    }
+
+    public void setBiology(double biology) {
+        this.biology = biology;
+    }
+
+    public int getPhysics() {
+        return physics;
+    }
+
+    public void setPhysics(int physics) {
+        this.physics = physics;
+    }
+
+    public double getChemistry() {
+        return chemistry;
+    }
+
+    public void setChemistry(double chemistry) {
+        this.chemistry = chemistry;
+    }
+
+    public int getBusinessStudies() {
+        return businessStudies;
+    }
+
+    public void setBusinessStudies(int businessStudies) {
+        this.businessStudies = businessStudies;
+    }
+
+    public int getFinance() {
+        return finance;
+    }
+
+    public void setFinance(int finance) {
+        this.finance = finance;
+    }
+
+    public int getAccountancy() {
+        return accountancy;
+    }
+
+    public void setAccountancy(int accountancy) {
+        this.accountancy = accountancy;
+    }
+
+    public int getTotalMarks() {
+        return totalMarks;
+    }
+
+    public void setTotalMarks(int totalMarks) {
+        this.totalMarks = totalMarks;
+    }
+}
