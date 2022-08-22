91b1279c4e41cf02de2be9f9531ea73a8c0a6c37
Neeraj7979 <Imnsinha77@outlook.com>
2022-08-22 09:46:44 +0530

    Implementing calculate method for all steam student

--- src/com/marks/StudentMarksImpl.java
+++ src/com/marks/StudentMarksImpl.java
@@ -3,9 +3,15 @@ package com.marks;
 public class StudentMarksImpl {
     public static void main(String[] args) {
         StudentMarks s1=new StudentMarks(70,80,87.6);
-
-        double totalMarks= s1.calculateMarks(70,80,87.6);
-        System.out.println("totalMarks = " + totalMarks);
+        System.out.println("Total Marks of Medical steam student s1");
+        double totalMarksMedical= s1.calculateMarks(70,80,87.6);
+        System.out.println("totalMarks = " + totalMarksMedical);
+        StudentMarks s2=new StudentMarks(70,86,86);
+        double totalMarksNonMedical=s2.calculateMarks(70,86,86);
+        System.out.println("Total Marks of Non Medical steam student s2");
+        System.out.println("totalMarks = " + totalMarksNonMedical);
+        StudentMarks s3= new StudentMarks(85,67,88.5f);
+        System.out.println("Total Marks Business stream : " +s3.calculateMarks(85,67,86.5f));
     }
 
 
